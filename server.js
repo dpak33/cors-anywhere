e22fc4957f4da5a2798a04de659371ef3fbad8ab
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-05 16:48:53 +0100

    y

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var originWhitelist = [];
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -44,6 +44,6 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
-}).listen(port, host, function() {
+}).listen(port, host, function () {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
